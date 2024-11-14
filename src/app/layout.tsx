12e2be7f89bf66e4fbdf3bb6b153c0122a8c87a7
Lucas Souza <alfsouza708@gmail.com>
2024-11-13 22:26:18 -0300

    chore: remove landing excess stores

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,30 +10,18 @@ import { ThemeProvider } from '@mui/material/styles';
 import { type ReactNode, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 
-import RTL from '@/app/(DashboardLayout)/layout/shared/customizer/RTL';
 import LoadingComponent from '@/components/loadingComponent';
-import { NextAuthProvider } from '@/lib/providers';
-import { useSelector } from '@/store/hooks';
-import type { AppState } from '@/store/store';
 import { store } from '@/store/store';
-import { NextAppDirEmotionCacheProvider } from '@/utils/theme/EmotionCache';
 import { ThemeSettings } from '@/utils/theme/Theme';
 
 export function MyApp({ children }: { children: React.ReactNode }) {
   const theme = ThemeSettings();
 
-  const customizer = useSelector((state: AppState) => state.customizer);
-
   return (
-    <NextAppDirEmotionCacheProvider options={{ key: 'modernize' }}>
-      <ThemeProvider theme={theme}>
-        <RTL direction={customizer.activeDir}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <NextAuthProvider>{children}</NextAuthProvider>
-        </RTL>
-      </ThemeProvider>
-    </NextAppDirEmotionCacheProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
   );
 }
 
