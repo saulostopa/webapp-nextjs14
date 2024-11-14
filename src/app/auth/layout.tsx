95f382bd073f7397ad056c94b1a864ae969f4ce0
Lucas Souza <alfsouza708@gmail.com>
2024-11-14 12:46:08 -0300

    fix: erro de session

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,40 +2,22 @@
 
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import { type ReactElement, type ReactNode, useEffect, useState } from 'react';
+import { type ReactElement, type ReactNode, useEffect } from 'react';
 
 import AccessPage from '@/components/accessPage';
-import LoadingComponent from '@/components/loadingComponent';
 import { AuthProvider } from '@/contexts/auth';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const router = useRouter();
-  const { data: session, status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      router.push('/');
-    },
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(true), 3000);
-  }, []);
+  const { data: session, status } = useSession();
 
   useEffect(() => {
     if (status === 'authenticated') router.push('/user/profile');
   }, [status, router]);
 
   return (
-    <div>
-      {loading ? (
-        <AuthProvider session={session!}>
-          <AccessPage>{children as ReactElement}</AccessPage>
-        </AuthProvider>
-      ) : (
-        <LoadingComponent />
-      )}
-    </div>
+    <AuthProvider session={session!}>
+      <AccessPage>{children as ReactElement}</AccessPage>
+    </AuthProvider>
   );
 }
